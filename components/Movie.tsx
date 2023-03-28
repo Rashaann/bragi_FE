ea5904bdef7b027e86890a59856774d9dc6513c0
zAk <rashaandesnoyers@gmail.com>
2023-03-28 04:17:41 +0200

    Dynamic setup of movies urls

--- components/Movie.tsx
+++ components/Movie.tsx
@@ -30,20 +30,58 @@ export default function Movie() {
                 <p className={styles.loading}>Loading...</p>
             </div>
         </main>
-  )
+    );
 
-    
-    
+
+    // Initialisation of state variables for each "players"
+    const [vf, setVf] = useState<string>('');
+    const [vostfr, setVostfr] = useState<string>('');
+    const [vo, setVo] = useState<string>('');
+
+ 
+
 
     useEffect(() => {
         fetch("https://bragi-be.vercel.app/movies/all")
         .then(response => response.json())
         .then(data => {
             // console.log('test => ', router.query)
-          data.list.map((el:{id:string}) => {
+          data.list.map((el:{id:string, pcloudIds:{vf: string, vostfr: string, vo: string}}) => {
               if(el.id === router.query.movie){
                 setArticlesList(el);
                 setIsDataLoaded(true);
+
+
+
+
+
+            console.log('id => ', el.pcloudIds.vf)
+            if(el.pcloudIds.vf){
+                fetch(`https://bragi-be.vercel.app/movies/movieUrl/${el.pcloudIds.vf}`)
+                .then(response => response.json())
+                .then(movieVf => {
+                    console.log(movieVf);
+                    setVf(movieVf.url);
+                })
+            }
+
+            if(el.pcloudIds.vostfr){
+                fetch(`https://bragi-be.vercel.app/movies/movieUrl/${el.pcloudIds.vostfr}`)
+                .then(response => response.json())
+                .then(movieVostfr => {
+                    setVf(movieVostfr.url);
+                })
+            }
+
+            if(el.pcloudIds.vo){
+                fetch(`https://bragi-be.vercel.app/movies/movieUrl/${el.pcloudIds.vo}`)
+                .then(response => response.json())
+                .then(movieVo => {
+                    setVf(movieVo.url);
+                })
+            }
+
+                // console.log(el.pcloudIds);
             }
           })
         });
@@ -57,6 +95,8 @@ export default function Movie() {
             <meta name="description" content="Free streaming website" />
             <meta name="viewport" content="width=device-width, initial-scale=1" />
             <link rel="icon" href="https://res.cloudinary.com/dldeqai4u/image/upload/v1679305932/bragi/icon_izqe4d.png" />
+            <link href="https://vjs.zencdn.net/8.0.4/video-js.css" rel="stylesheet" />
+            <script src="https://vjs.zencdn.net/ie8/1.1.2/videojs-ie8.min.js"></script>
         </Head>
 
         <Header />
@@ -79,14 +119,26 @@ export default function Movie() {
                 
                 <div className={styles.stream}>
                     <div className={styles.icons}>
-                        <img src="https://res.cloudinary.com/dldeqai4u/image/upload/v1679006146/bragi/french_flag_xzuxke.png" className={styles.languageIcon} onClick={() => setLink(articlesList.link.vf[0])} />
-                        <img src="https://res.cloudinary.com/dldeqai4u/image/upload/v1679006146/bragi/vostfr_tzzr4h.jpg" className={styles.languageIcon} onClick={() => setLink(articlesList.link.vostfr[0])} />
-                        <img src="https://res.cloudinary.com/dldeqai4u/image/upload/v1679006146/bragi/english_flag_mlp7wy.png" className={styles.languageIcon} onClick={() => setLink(articlesList.link.vo[0])} />
+                        <img src="https://res.cloudinary.com/dldeqai4u/image/upload/v1679006146/bragi/french_flag_xzuxke.png" className={styles.languageIcon} onClick={() => setLink(vf)} />
+                        <img src="https://res.cloudinary.com/dldeqai4u/image/upload/v1679006146/bragi/vostfr_tzzr4h.jpg" className={styles.languageIcon} onClick={() => setLink(vostfr)} />
+                        <img src="https://res.cloudinary.com/dldeqai4u/image/upload/v1679006146/bragi/english_flag_mlp7wy.png" className={styles.languageIcon} onClick={() => setLink(vo)} />
                     </div>
                     {(link==='')||(link===undefined)?
                     <div className={styles.chooseLink}>Please choose the version to display</div>:
                     <div className={styles.backStream}>
-                        <iframe id='test' src={link} style={{borderWidth: 0}} className={styles.test} width={800} height={450} allowFullScreen ></iframe>
+                        <iframe id='test' src={vf} style={{borderWidth: 0}} className={styles.test} width={800} height={450} allowFullScreen ></iframe>
+                        {/* <video
+                            id="my-video"
+                            controls
+                            preload="auto"
+                            width="640"
+                            height="264"
+                            poster="MY_VIDEO_POSTER.jpg"
+                            data-setup="{}"
+                        >
+                            <source src={vf} type="video/mp4" />
+                        </video>
+                        <script src="https://vjs.zencdn.net/8.0.4/video.min.js"></script> */}
                     </div>}
                 </div>
             </div>:
@@ -105,9 +157,9 @@ export default function Movie() {
                 
                 <div className={styles.smStream}>
                     <div className={styles.smIcons}>
-                        <img src="https://res.cloudinary.com/dldeqai4u/image/upload/v1679006146/bragi/french_flag_xzuxke.png" className={styles.smLanguageIcon} onClick={() => setLink(articlesList.link.vf[0])} />
-                        <img src="https://res.cloudinary.com/dldeqai4u/image/upload/v1679006146/bragi/vostfr_tzzr4h.jpg" className={styles.smLanguageIcon} onClick={() => setLink(articlesList.link.vostfr[0])} />
-                        <img src="https://res.cloudinary.com/dldeqai4u/image/upload/v1679006146/bragi/english_flag_mlp7wy.png" className={styles.smLanguageIcon} onClick={() => setLink(articlesList.link.vo[0])} />
+                        <img src="https://res.cloudinary.com/dldeqai4u/image/upload/v1679006146/bragi/french_flag_xzuxke.png" className={styles.smLanguageIcon} onClick={() => setLink(vf)} />
+                        <img src="https://res.cloudinary.com/dldeqai4u/image/upload/v1679006146/bragi/vostfr_tzzr4h.jpg" className={styles.smLanguageIcon} onClick={() => setLink(vostfr)} />
+                        <img src="https://res.cloudinary.com/dldeqai4u/image/upload/v1679006146/bragi/english_flag_mlp7wy.png" className={styles.smLanguageIcon} onClick={() => setLink(vo)} />
                     </div>
                     {(link==='')||(link===undefined)?
                     <div className={styles.smChooseLink}>Please choose the version to display</div>:
