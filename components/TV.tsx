abb7afa667131f062217d299635e44718a633e5a
zAk <rashaandesnoyers@gmail.com>
2023-04-12 01:17:43 +0200

    Comment of tv tests

--- components/TV.tsx
+++ components/TV.tsx
@@ -63,7 +63,7 @@ export default function TV() {
           </div>
         </div>
 
-        <div className={styles.description}>
+        {/* <div className={styles.description}>
         <video
             id="my-video"
             controls
@@ -77,7 +77,7 @@ export default function TV() {
           <script src="https://unpkg.com/video.js/dist/video.js"></script>
           <script src="https://unpkg.com/videojs-contrib-hls/dist/videojs-contrib-hls.js"></script>
 
-        </div>
+        </div> */}
       </main>
 
       <Footer />
